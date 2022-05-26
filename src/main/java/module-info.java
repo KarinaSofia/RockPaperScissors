07e92e23122994911f6cb731c4c4d7ed26aa9330
KarinaSofia <104159293+KarinaSofia@users.noreply.github.com>
2022-05-25 21:06:04 -0400

    jUnit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.rockpaperscissors {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.junit.jupiter.api;
 
 
     opens com.example.rockpaperscissors to javafx.fxml;
